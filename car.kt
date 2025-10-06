ae474ce02d6696cde593790de308606783b2fb18
Esteban Amores <estebanamoresbarrantes@gmail.com>
2025-10-06 17:20:53 -0600

    Creacion de los metodos start_engine & accelerate

--- car.kt
+++ car.kt
@@ -1,30 +1,51 @@
-interface IVehicle{  
-  fun start_engine() //print a message the vehicle was started and the acceleration is 0
-  fun accelerate(acceleration: Int) //print a message the current acceleration of the vehicle, you need to sum the new acceleration
-  fun brake() //print a message the vehicle is stopped and the acceleration is 0
-  fun turn_off_engine() //print a message the vehicle was turned off with acceleration = 0
+interface IVehicle{
+  //print a message the vehicle was started and the acceleration is 0  
+  fun start_engine()
+  //print a message the current acceleration of the vehicle, you need to sum the new acceleration
+  fun accelerate(acceleration: Int)
+  //print a message the vehicle is stopped and the acceleration is 0
+  fun brake()
+  //print a message the vehicle was turned off with acceleration = 0
+  fun turn_off_engine()
 }
 
 class Car: IVehicle{
 
-  private var acceleration = 0;
-  private var turnedOn = true;
+  private var _acceleration = 0;
+  private var _turnedOn = false;
 
-   constructor(acceleration: Int, turnedOn: Boolean){
+  constructor(acceleration: Int, turnedOn: Boolean){
     this._acceleration = acceleration;
     this._turnedOn = turnedOn; 
-   }
-  
-  override fun brake{ //Jorjan
-    acceleration = 0;
-    println("The vehicle is stopped. The acceletarion is $acceleration")
   }
   
-  override fun turn_off_engine { //Jorjan
-    acceleration = 0; 
-    turnedOn = false; 
-    if turnedOn == false {
-      println("The vehicle was turned off. The acceleration is $acceleration ")
+  //Esteban
+  override fun start_engine() { 
+    _acceleration = 0
+    _turnedOn = true
+    println("The vehicle was started and the acceleration is $_acceleration")
+  }
+
+  //Esteban
+  override fun accelerate(acceleration: Int) { 
+    if (_turnedOn) {
+        this._acceleration += (acceleration + 5)
+        println("The current acceleration of the vehicle is $_acceleration")
+    } else {
+        println("Cannot accelerate. The vehicle is off.")
+    }
+  }
+
+  override fun brake() { //Jorjan
+    _acceleration = 0;
+    println("The vehicle is stopped. The acceleration is $_acceleration")
+  }
+  
+  override fun turn_off_engine() { //Jorjan
+    _acceleration = 0; 
+    _turnedOn = false; 
+    if (_turnedOn == false) {
+      println("The vehicle was turned off. The acceleration is $_acceleration ")
     }
   }
 }
