18e355a18ae5cd4d5c3495ae38fcbcbc7cc49aec
Erick Murillo <ermurilloba@est.utn.ac.cr>
2025-10-07 12:37:43 -0600

    tarea

--- erick.kt
+++ erick.kt
@@ -6,5 +6,8 @@ interface IVehicle{
 }
 
 class Car: IVehicle{
-  
+    fun start_engine() //print a message the vehicle was started and the acceleration is 0
+  fun accelerate(acceleration: Int) //print a message the current acceleration of the vehicle, you need to sum the new acceleration
+  fun brake() //print a message the vehicle is stopped and the acceleration is 0
+  fun turn_off_engine() //print a message the vehicle was turned off with acceleration = 0
 }
