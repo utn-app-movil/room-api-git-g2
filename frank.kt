3d3719c2798752c984951a73a05b5ba97fd0b5e9
Frankms123 <frmorasa@est.utn.ac.cr>
2025-10-07 17:47:00 -0600

    Update frank.kt
    Tarea Dispositivos Móviles

--- frank.kt
+++ frank.kt
@@ -5,6 +5,37 @@ interface IVehicle{
   fun turn_off_engine() //print a message the vehicle was turned off with acceleration = 0
 }
 
-class Car: IVehicle{
-  
+class Car : IVehicle {
+    var acc = 0
+    
+    override fun start_engine() {
+        acc = 0
+        println("Engine started - acceleration is $acc")
+    }
+
+    override fun accelerate(acceleration: Int) {
+        acc = acc + acceleration
+        println("Accelerating - current acceleration: $acc")
+    }
+
+    override fun brake() {
+        acc = 0
+        println("Braking - vehicle stopped, acceleration is $acc")
+    }
+
+    override fun turn_off_engine() {
+        acc = 0
+        println("Engine off - acceleration is $acc")
+    }
+}
+
+fun main() {
+    val vehicle = Car()
+    
+    vehicle.start_engine()
+    vehicle.accelerate(10)
+    vehicle.accelerate(20)
+    vehicle.accelerate(5)
+    vehicle.brake()
+    vehicle.turn_off_engine()
 }
