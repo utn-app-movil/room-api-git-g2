51e7cc1e5e3d1c69f5f96529dbcd0588ba94f084
Sergio Quesada <sqada2804@gmail.com>
2025-10-09 23:25:58 -0600

    subiendo cambios a rama sergio

--- car.kt
+++ car.kt
@@ -1,4 +1,6 @@
-interface IVehicle{  
+import java.io.IO.println
+
+interface IVehicle{
   fun start_engine() //print a message the vehicle was started and the acceleration is 0
   fun accelerate(acceleration: Int) //print a message the current acceleration of the vehicle, you need to sum the new acceleration
   fun brake() //print a message the vehicle is stopped and the acceleration is 0
