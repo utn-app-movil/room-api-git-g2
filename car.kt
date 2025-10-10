b45313386673fa0d83fa86f336b73e66f86aa3e6
Nan141217 <mafesolanoro@est.utn.ac.cr>
2025-10-09 23:45:40 -0600

    me encargue de mejorar los metodos de implementacion

--- car.kt
+++ car.kt
@@ -11,31 +11,44 @@ class Car : IVehicle {
     private var engineOn = false
 
     override fun start_engine() {
-        engineOn = true
-        acceleration = 0
-        println("The car engine was started. Acceleration is $acceleration.")
-    }
-
-    override fun accelerate(acceleration: Int) {
-        if (engineOn) {
-            this.acceleration += acceleration
-            println("The car accelerates. Current acceleration: ${this.acceleration}.")
-        } else {
-            println("Cannot accelerate. The engine is off.")
+        interface IVehicle {
+            fun start_engine() // print a message: the vehicle was started and the acceleration is 0
+            fun accelerate(acceleration: Int) // print a message: the current acceleration of the vehicle, you need to sum the new acceleration
+            fun brake() // print a message: the vehicle is stopped and the acceleration is 0
+            fun turn_off_engine() // print a message: the vehicle was turned off with acceleration = 0
         }
-    }
 
-    override fun brake() {
-        acceleration = 0
-        println("The car stopped. Acceleration is $acceleration.")
-    }
+        class Car : IVehicle {
 
-    override fun turn_off_engine() {
-        if (acceleration == 0) {
-            engineOn = false
-            println("The car engine was turned off.")
-        } else {
-            println("Cannot turn off the engine while the car is moving.")
+            private var acceleration = 0
+            private var engineOn = false
+
+            override fun start_engine() {
+                engineOn = true
+                acceleration = 0
+                println("The car engine was started. Acceleration is $acceleration.")
+            }
+
+            override fun accelerate(acceleration: Int) {
+                if (engineOn) {
+                    this.acceleration += acceleration
+                    println("The car accelerates. Current acceleration: ${this.acceleration}.")
+                } else {
+                    println("Cannot accelerate. The engine is off.")
+                }
+            }
+
+            override fun brake() {
+                acceleration = 0
+                println("The car stopped. Acceleration is $acceleration.")
+            }
+
+            override fun turn_off_engine() {
+                if (acceleration == 0) {
+                    engineOn = false
+                    println("The car engine was turned off.")
+                } else {
+                    println("Cannot turn off the engine while the car is moving.")
+                }
+            }
         }
-    }
-}
